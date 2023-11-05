cb58e2815f8d27a9f7f6edda5857c1b1994ea8ca
hg-jang <wkdgusrhkd@gmail.com>
2023-11-05 22:09:38 +0900

    style: footer

--- src/lib/components/common/Footer.svelte
+++ src/lib/components/common/Footer.svelte
@@ -2,18 +2,13 @@
   import { Kbd } from 'flowbite-svelte';
 </script>
 
-<footer class="px-8 py-12 text-[var(--text-light)] dark:text-[var(--text-dark)]">
+<footer
+  class="px-8 py-12 text-sm text-[var(--text-muted)] dark:text-[var(--text-muted)] text-center"
+>
   <p>
-    &copy;2023, Built by <Kbd
-      class="px-2 py-1.5 text-[var(--primary-blue)] dark:text-[var(--primary-blue-dark)]"
-      >king-bada</Kbd
-    > with
-    <a
-      class="tool dark:text-[var(--primary-blue-dark)]"
-      href="https://kit.svelte.dev/"
-      target="_blank">sveltekit</a
-    >,
-    <a
+    &copy;&nbsp;2023, Built by bada with
+    <a class="text-inherit" href="https://kit.svelte.dev/" target="_blank">sveltekit</a>.
+    <!-- <a
       class="tool dark:text-[var(--primary-blue-dark)]"
       href="https://tailwindcss.com/"
       target="_blank">tailwindcss</a
@@ -22,13 +17,13 @@
       class="tool dark:text-[var(--primary-blue-dark)]"
       href="https://flowbite-svelte.com/"
       target="_blank">flowbite-svelte</a
-    >.
+    >. -->
   </p>
 </footer>
 
 <style>
-  .tool {
+  /* .tool {
     color: var(--primary-blue);
     text-decoration: underline;
-  }
+  } */
 </style>
