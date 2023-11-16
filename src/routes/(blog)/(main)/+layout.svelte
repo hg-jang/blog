13fd40c02f2fabe561877a4023980e372572aa76
hg-jang <wkdgusrhkd@gmail.com>
2023-11-16 23:48:52 +0900

    fix: 메인 헤더 아이콘 삭제

--- src/routes/(blog)/(main)/+layout.svelte
+++ src/routes/(blog)/(main)/+layout.svelte
@@ -51,27 +51,6 @@
         <br />
         요즘엔 Nest에 관심이 많습니다.
       </p>
-      <div class="flex flex-row">
-        <!-- Github icon -->
-        <a href={LINK.github} target="_blank" class="blog-social-button">
-          <img src="{base}/icons/github-mark-white.png" class="hidden dark:block" alt="github" />
-          <img src="{base}/icons/github-mark.png" class="block dark:hidden" alt="github" />
-        </a>
-
-        <!-- Gmail icon -->
-        <a
-          on:click|preventDefault={toastTrigger}
-          class="blog-social-button bg-[var(--bg-dark)] dark:bg-[var(--bg-light)]"
-          href="{base}/"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
-            ><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              class="fill-[var(--text-dark)] dark:fill-[var(--text-light)]"
-              d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
-            /></svg
-          >
-        </a>
-      </div>
     </div>
   </div>
 </header>
