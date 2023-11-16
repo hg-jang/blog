c091bd6b901f3cfef62fb8db10ec3c189d27b536
hg-jang <wkdgusrhkd@gmail.com>
2023-11-16 23:53:13 +0900

    refactor: 포스트 카드 컴포넌트 추출

---
+++ src/lib/components/card/PostCard.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { base } from '$app/paths';
+  import { formatDate } from '$lib/utils/date';
+
+  /**
+   * @type {import('$lib/types').Post}
+   */
+  export let post;
+</script>
+
+<li
+  class="
+				p-2 bg-white border border-solid border-[var(--border-light)] rounded cursor-pointer
+				text-[var(--text-light)] dark:text-[var(--text-dark)]
+				dark:bg-[var(--bg-dark)] dark:border-[var(--border-dark)] hover:scale-105
+			"
+>
+  <a href="{base}/{post.slug}">
+    <h3 class="text-xl font-medium">{post.title}</h3>
+    <p class="mb-1 text-xs text-muted">{formatDate(post.date)}</p>
+    <p class="text-base text-muted">{post.description}</p>
+  </a>
+</li>
--- src/routes/(blog)/(main)/+page.svelte
+++ src/routes/(blog)/(main)/+page.svelte
@@ -8,6 +8,7 @@
   import CategoryNav from '$lib/components/common/CategoryNav.svelte';
 
   import { Pagination } from 'flowbite-svelte';
+  import PostCard from '$lib/components/card/PostCard.svelte';
 
   export let data;
 
@@ -118,19 +119,7 @@
 <!-- posts -->
 <ul class="flex flex-col gap-y-5">
   {#each pagnationedPosts as post}
-    <li
-      class="
-				p-2 bg-white border border-solid border-[var(--border-light)] rounded cursor-pointer
-				text-[var(--text-light)] dark:text-[var(--text-dark)]
-				dark:bg-[var(--bg-dark)] dark:border-[var(--border-dark)] hover:scale-105
-			"
-    >
-      <a href="{base}/{post.slug}">
-        <h3 class="text-xl font-medium">{post.title}</h3>
-        <p class="mb-1 text-xs text-muted">{formatDate(post.date)}</p>
-        <p class="text-base text-muted">{post.description}</p>
-      </a>
-    </li>
+    <PostCard {post} />
   {/each}
 </ul>
 
